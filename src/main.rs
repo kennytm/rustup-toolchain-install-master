63f31a0762a1517b0d7205bacaddfeee20a7b7d4
kennytm <kennytm@gmail.com>
2018-06-07 17:08:23 +0800

    cargo fmt.

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,18 @@ extern crate tee;
 extern crate tempdir;
 extern crate xz2;
 
+use std::borrow::Cow;
 use std::env::set_current_dir;
 use std::fs::{create_dir_all, rename};
 use std::iter::once;
 use std::path::{Path, PathBuf};
 use std::process::exit;
-use std::borrow::Cow;
 use std::time::Duration;
 
 use failure::Error;
 use pbr::{ProgressBar, Units};
-use reqwest::{Client, ClientBuilder, Proxy};
 use reqwest::header::ContentLength;
+use reqwest::{Client, ClientBuilder, Proxy};
 use structopt::StructOpt;
 use tar::Archive;
 use tee::TeeReader;
@@ -29,24 +29,33 @@ use xz2::read::XzDecoder;
 
 #[derive(StructOpt, Debug)]
 struct Args {
-    #[structopt(help = "full commit hashes of the rustc builds; all 40 digits are needed",
-                raw(required = "true"))]
+    #[structopt(
+        help = "full commit hashes of the rustc builds; all 40 digits are needed",
+        raw(required = "true")
+    )]
     commits: Vec<String>,
 
-    #[structopt(short = "a", long = "alt",
-                help = "download the alt build instead of normal build")]
+    #[structopt(
+        short = "a", long = "alt", help = "download the alt build instead of normal build"
+    )]
     alt: bool,
 
-    #[structopt(short = "s", long = "server",
-                help = "the server path which stores the compilers",
-                default_value = "https://s3-us-west-1.amazonaws.com/rust-lang-ci2")]
+    #[structopt(
+        short = "s",
+        long = "server",
+        help = "the server path which stores the compilers",
+        default_value = "https://s3-us-west-1.amazonaws.com/rust-lang-ci2"
+    )]
     server: String,
 
     #[structopt(short = "i", long = "host", help = "the triples of host platform")]
     host: Option<String>,
 
-    #[structopt(short = "t", long = "targets",
-                help = "additional target platforms to install, besides the host platform")]
+    #[structopt(
+        short = "t",
+        long = "targets",
+        help = "additional target platforms to install, besides the host platform"
+    )]
     targets: Vec<String>,
 
     #[structopt(short = "p", long = "proxy", help = "the HTTP proxy for all download requests")]
@@ -158,7 +167,8 @@ fn run() -> Result<(), Error> {
     let host = args.host.as_ref().map(|s| &**s).unwrap_or(env!("HOST"));
     let rustc_filename = format!("rustc-nightly-{}", host);
 
-    let rust_std_targets = args.targets
+    let rust_std_targets = args
+        .targets
         .iter()
         .map(|s| &**s)
         .chain(once(host))
